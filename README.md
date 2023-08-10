9d5e5d61f2cad4a5fe7b26e010467e1a30576c03
Zidan Herlangga <133259343+zidanherlangga04@users.noreply.github.com>
2023-08-10 15:19:22 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Website Kafe
+
+**Live Demo** :
+[Click here](https://kafein.netlify.app)
+
+## Snapshot
+
+![Snapshot](Screenshot.png)
+
+## Library
+
+- Google Fonts [Link](https://fonts.google.com)
+- Feather Icons [Link](https://feathericons.com/)
+- Typed JS [Link](https://typeitjs.com)
+- AOS (Animation On Scroll) [Link](https://michalsnik.github.io/aos/)
--- js/script.js
+++ js/script.js
@@ -40,10 +40,32 @@ let typing = new Typed("#typed", {
 });
 
 // Animasi loading
-const loader = document.getElementById("preloader");
-window.addEventListener("load", function () {
-  loader.style.display = "none";
-});
+// const loader = document.getElementById("preloader");
+// window.addEventListener("load", function () {
+//   loader.style.display = "none";
+// });
 
-let fullName = document.getElementById("name");
-window.addEventListener;
+// let fullName = document.getElementById("name");
+// window.addEventListener;
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
